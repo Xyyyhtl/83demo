ee2ef28926c64790894dab616aa4384745d104d7
Liyong <763739903@qq.com>
2019-09-18 22:03:08 +0800

    登录框居中完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>我是登录</div>
+  <div class="login">
+      <!-- 使用elementui 卡片组件 el-card-->
+      <el-card class="login-card"></el-card>
+  </div>
 </template>
 
 <script>
@@ -8,6 +11,22 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+/* 想用less在组件内写样式 就得定义leng="less"属性
+scoped解决组件内使用同样选择器，且该属性只对当前组件有效 */
+.login {
+    background-image: url('../../assets/img/login_bg.jpg');
+    height: 100vh;
+    background-size: cover;
+    // 使用flex布局
+    display: flex;
+    // 水平居中
+    justify-content: center;
+    // 垂直居中
+    align-items: center;
+    .login-card {
+        width: 440px;
+        height: 340px;
+    }
+}
 </style>
