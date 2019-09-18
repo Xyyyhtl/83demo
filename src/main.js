92190ff21fb66d1c564684b985e561c0bcf39571
Liyong <763739903@qq.com>
2019-09-18 19:44:14 +0800

    黑马头条PC-初始化项目-安装ElementUI

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui' // 1,引入下载好的elementui插件
+Vue.use(ElementUI) // 2，全局注册elementui插件
 Vue.config.productionTip = false
 
 new Vue({
