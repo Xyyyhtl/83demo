f86a007060af3cad515ec1e1ea055afd173bf289
Liyong <763739903@qq.com>
2019-09-18 21:01:21 +0800

    黑马头条PC-初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui' // 1,引入下载好的elementui插件
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './styles/index.less'
 Vue.use(ElementUI) // 2，全局注册elementui插件
 Vue.config.productionTip = false
 
