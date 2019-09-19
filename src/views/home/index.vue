8cf71272ac1d7d6b579ec598dc2aee9652b1d24f
Liyong <763739903@qq.com>
2019-09-19 15:31:29 +0800

    主页页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>我是首页</div>
+  <!-- 定义超级大容器 -->
+  <el-container>
+    <!-- 放置左侧列表容器 -->
+    <el-aside>左侧列表</el-aside>
+    <!-- 右侧大容器 -->
+    <el-container>
+      <!-- 顶部导航栏容器 -->
+      <el-header>头部导航</el-header>
+      <!-- 中部容器 -->
+      <el-main>
+        <!-- 二级路由容器 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
