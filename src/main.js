4a071a6e42f076ac76c2c97ab01f6f54f0a80ba5
Liyong <763739903@qq.com>
2019-09-19 11:59:19 +0800

    axios配置和全局设置

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui' // 1,引入下载好的elementui插件
+import ElementUI from 'element-ui' // ①,引入下载好的elementui插件
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
-import './styles/index.less'
-Vue.use(ElementUI) // 2，全局注册elementui插件
+import './styles/index.less' //
+import axios from 'axios' // 1，引入axios配置
+Vue.prototype.$axios = axios // 2，讲axios共享给所有得实例或组件使用
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'// 3,将地址得常态值设置给baseURL
+Vue.use(ElementUI) // ②，全局注册elementui插件
 Vue.config.productionTip = false
 
 new Vue({
