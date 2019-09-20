6ed855cc33904244b349e664b642d6eac834b6bd
Liyong <763739903@qq.com>
2019-09-21 01:30:31 +0800

    全局注册插件的使用方式Vue.use

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+// 实现整体普通组件得统一注册
+import layoutAside from './home/layout-aside' // 左侧导航普通组件引入
+import layoutHeader from './home/layout-header' // 头部导航普通组件引入
+export default {
+  install (Vue) {
+    // 调用install方法 会自动传入Vue对象
+    // 组件可以在实例化之前注册组件
+    Vue.component('layout-aside', layoutAside) // 全局注册左侧导航组件
+    Vue.component('layout-header', layoutHeader) // 全局注册头部导航组件
+  }
+}
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,14 +4,14 @@
     <!-- 放置左侧列表容器 -->
     <el-aside style="width:200px">
       <!-- 左侧导航普通组件 -->
-      <layyou-aside></layyou-aside>
+      <layout-aside></layout-aside>
     </el-aside>
     <!-- 右侧大容器 -->
     <el-container>
       <!-- 顶部导航栏容器 -->
       <el-header>
         <!-- 导航头部组件 -->
-        <layyou-header></layyou-header>
+        <layout-header></layout-header>
       </el-header>
       <!-- 中部容器 -->
       <el-main>
@@ -23,16 +23,13 @@
 </template>
 
 <script>
-import layyouAside from '../../components/home/layout-aside' // 首页组件
-import layoutHeader from '../../components/home/layout-header' // 导航二级组件
 export default {
   components: {
-    'layyou-aside': layyouAside,
-    'layyou-header': layoutHeader
+
   }
 }
 </script>
 
 <style>
-height: ;
+
 </style>
