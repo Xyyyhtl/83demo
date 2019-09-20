687a7bbe2368e0415fc9efc55e2e12e96d7598c9
Liyong <763739903@qq.com>
2019-09-20 22:47:36 +0800

    头部导航及下拉菜单完成

---
+++ src/components/home/layout-header.vue
@@ -0,0 +1,59 @@
+<template>
+<!-- el-row包el-col/ 下拉菜单el-dropdown 所用组件 -->
+<!-- align="middle" flex垂直居中 -->
+  <el-row class="layout-header" align="middle" type="flex" justify="space-between">
+    <!-- 左侧 -->
+    <el-col :span="6">
+      <div class="grid-content bg-purple">
+        <i class="el-icon-s-unfold"></i>
+        <span class="title">江苏传智播客教育科技股份有限公司</span>
+      </div>
+    </el-col>
+    <!-- 右侧 -->
+    <el-col :span="4">
+        <!-- 用户头像 -->
+      <img class="head-img" src="../../assets/img/avatar.jpg" alt />
+      <!-- 下拉菜单组件 -->
+      <el-dropdown trigger="click">
+        <span class="el-dropdown-link">
+          用户名
+          <i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
+        <!-- 具名插槽 -->
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>个人信息</el-dropdown-item>
+          <el-dropdown-item>git地址</el-dropdown-item>
+          <el-dropdown-item>退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.layout-header {
+    height: 50px;
+    .el-icon-s-unfold {
+        font-size: 22px;
+    }
+    .title {
+        // 文字基线与图片顶对齐
+        vertical-align: top;
+        margin-left: 8px;
+        font-weight: 500;
+        font-size: 18px;
+    }
+    .head-img {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        // 文字基线与图片中基线顶对齐
+        vertical-align: middle;
+        margin-right: 12px;
+    }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,14 +2,17 @@
   <!-- 定义超级大容器 -->
   <el-container>
     <!-- 放置左侧列表容器 -->
-    <el-aside>
+    <el-aside style="width:200px">
       <!-- 左侧导航普通组件 -->
       <layyou-aside></layyou-aside>
     </el-aside>
     <!-- 右侧大容器 -->
     <el-container>
       <!-- 顶部导航栏容器 -->
-      <el-header>头部导航</el-header>
+      <el-header>
+        <!-- 导航头部组件 -->
+        <layyou-header></layyou-header>
+      </el-header>
       <!-- 中部容器 -->
       <el-main>
         <!-- 二级路由容器 -->
@@ -20,13 +23,16 @@
 </template>
 
 <script>
-import layyouAside from '../../components/home/layout-aside'
+import layyouAside from '../../components/home/layout-aside' // 首页组件
+import layoutHeader from '../../components/home/layout-header' // 导航二级组件
 export default {
   components: {
-    'layyou-aside': layyouAside
+    'layyou-aside': layyouAside,
+    'layyou-header': layoutHeader
   }
 }
 </script>
 
 <style>
+height: ;
 </style>
