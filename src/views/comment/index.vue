890ce5558cfc334ce0388434ce41c2589f7e195d
Liyong <763739903@qq.com>
2019-09-23 21:24:21 +0800

    table内布尔值得转文本处理完成

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -9,12 +9,12 @@
     <!-- el-table 表格 data属性是一个对象数组-->
     <el-table :data="list">
       <!-- el-table-column 是表格得列属性    align对齐方式 prop是键名或称字段名  label是表头 width属性来定义列宽。-->
+      <!-- 布尔值在table上显示不了 使用table属性formatter来处理-->
       <el-table-column width="1000px" prop="title" label="标题"></el-table-column>
-      <el-table-column align="center" prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column :formatter="statusFormatter" align="center" prop="comment_status" label="评论状态"></el-table-column>
       <el-table-column align="center" prop="total_comment_count" label="评论数"></el-table-column>
       <el-table-column align="center" prop="fans_comment_count" label="粉丝评论数"></el-table-column>
       <el-table-column align="center" label="操作"></el-table-column>
-
     </el-table>
   </el-card>
 </template>
@@ -36,6 +36,15 @@ export default {
       }).then(result => {
         this.list = result.data.results // 接收数据，results 评论列表对象  且赋值给list
       })
+    },
+    // 该table属性 有4个参数
+    // row 代表-- 所有行得数据得对象
+    // column 代表-- 列得数据对象
+    // cellValue 代表-- 当前列得布尔值
+    // index 代表-- 元素得索引
+    statusFormatter (row, column, cellValue, index) {
+      // row["comment_status"] ? '正常' : '关闭' 也可以
+      return cellValue ? '正常' : '关闭' // 当布尔值为true时显示文本正常，fasle为关闭
     }
   },
   // 使用钩子函数 调用获取数据列表
