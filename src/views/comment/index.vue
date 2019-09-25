269bd85698e921d5892b4bc9f66b56845b5b0093
Liyong <763739903@qq.com>
2019-09-25 14:09:04 +0800

    分页获取数据完成

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -3,7 +3,7 @@
   <el-card>
     <!-- 给card得插槽 -->
     <bread-crumb slot="header">
-    <!-- 给面包屑得 插槽 -->
+      <!-- 给面包屑得 插槽 -->
       <template slot="title">评论列表</template>
     </bread-crumb>
     <!-- el-table 表格 data属性是一个对象数组-->
@@ -11,23 +11,43 @@
       <!-- el-table-column 是表格得列属性    align对齐方式 prop是键名或称字段名  label是表头 width属性来定义列宽。-->
       <!-- 布尔值在table上显示不了 使用table属性formatter来处理-->
       <el-table-column width="1000px" prop="title" label="标题"></el-table-column>
-      <el-table-column :formatter="statusFormatter" align="center" prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column
+        :formatter="statusFormatter"
+        align="center"
+        prop="comment_status"
+        label="评论状态"
+      ></el-table-column>
       <el-table-column align="center" prop="total_comment_count" label="评论数"></el-table-column>
       <el-table-column align="center" prop="fans_comment_count" label="粉丝评论数"></el-table-column>
       <el-table-column align="center" label="操作">
         <!-- ② 使用emlementui 内得作用域插槽 父读取收子组件数据  emlementui已经做好了子组件 所以可以直接读取  -->
         <template slot-scope="obj">
           <!-- 操作里面的状态 需要根据评论状态 展示反值 当评论为关，操作显示打开 -->
-        <!-- ① （该子组件看不到，emlementui已经做好，直接可用）通过 作用域Scoped slot 可以获取到 row, column, $index 和 store（table 内部的状态管理）的数据，用法参考 demo。 -->
+          <!-- ① （该子组件看不到，emlementui已经做好，直接可用）通过 作用域Scoped slot 可以获取到 row, column, $index 和 store（table 内部的状态管理）的数据，用法参考 demo。 -->
           <el-button size="small" type="text">修改</el-button>
           <!--  当状态为true 操作为关闭   false是打开 -->
-          <el-button :style="{color: obj.row.comment_status ? '#909399' : '#F56C6C'}" @click="closeOropen(obj.row)" size="small" type="text">
+          <!-- 使用:style判断样式 -->
+          <el-button
+            :style="{color: obj.row.comment_status ? '#909399' : '#F56C6C'}"
+            @click="closeOropen(obj.row)"
+            size="small"
+            type="text"
+          >
             {{
             obj.row.comment_status ? "关闭" : "打开"
-            }}</el-button>
+            }}
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分页 页面结构 -->
+    <el-row type="flex" justify="center" style="margin:20px">
+      <!-- 分页 total代表总数据数据
+      :page-size单页多少数据
+      :current-page="page.currentPage"默认第几页
+      @current-change="changePage"监听页码改变事件  该事件改变时会传入参数（当前页码参数）-->
+      <el-pagination @current-change="changePage" :current-page="page.currentPage" :page-size="page.pageSize" background layout="prev, pager, next" :total="page.total"></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -35,18 +55,36 @@
 export default {
   data () {
     return {
-      list: []
+      list: [],
+      // 分页数据
+      page: {
+        // 总数据数量
+        total: 0,
+        // 默认第几页
+        currentPage: 1,
+        // 单页多少数据
+        pageSize: 10
+      }
     }
   },
+  // 方法
   methods: {
+    // 获取当前页吗方法
+    // @current-change该事件，会返回当前改变得页码 newPage接收最新得页面
+    changePage (newPage) {
+      // alert(newPage) // 获取当前页码
+      this.page.currentPage = newPage // 将当前最新得页码 给数据currentPage页码
+      this.getComment() // 调用获取列表方法
+    },
     // 获取评论列表数据
     getComment () {
       this.$axios({
-        url: '/articles', // 评论列表接口
-        params: { response_type: 'comment' } // 传参数才可以获取，评论列表数据，comment参数得值，获取评论字段
+        url: '/articles', // 评论列表接口   page: this.page.currentPage当前最新页码，  per_page: this.page.pageSize 当前单页得数据
+        params: { response_type: 'comment', page: this.page.currentPage, per_page: this.page.pageSize } // 传参数才可以获取，评论列表数据，comment参数得值，获取评论字段
         // params 是路径参数，也就是query参数
       }).then(result => {
         this.list = result.data.results // 接收数据，results 评论列表对象  且赋值给list
+        this.page.total = result.data.total_count // 获取到总数据，且赋值给总数据
       })
     },
     // 该table属性 有4个参数
@@ -60,11 +98,14 @@ export default {
     },
     // 打开或关闭 评论状态  接收表达式得传参 row行得数据
     closeOropen (row) {
-    //  获取评论状态得文本 true关闭 false打开
+      //  获取评论状态得文本 true关闭 false打开
       // let mess = row.comment_status ? '关闭' : '打开'
       // 使用emlementui内得方法$confirm方法即可打开消息提示
-      this.$confirm(`你确定${row.comment_status ? '关闭' : '打开'}评论？`, '龟儿子', {
-      }).then(() => {
+      this.$confirm(
+        `你确定${row.comment_status ? '关闭' : '打开'}评论？`,
+        '龟儿子',
+        {}
+      ).then(() => {
         // 确定是调用接口, 根据关闭评论或打开评论状态
         this.$axios({
           url: '/comments/status', // 地址
@@ -87,5 +128,4 @@ export default {
 </script>
 
 <style>
-
 </style>
